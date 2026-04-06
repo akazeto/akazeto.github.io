e41c5ac8b3e3b81a48332301b0adcc58879f97aa
akazeto <rlaenrl332@gmail.com>
2026-04-06 15:24:29 +0900

    40

--- assets/js/sticky-nav.js
+++ assets/js/sticky-nav.js
@@ -1,8 +1,9 @@
 const stickyNav = document.getElementById('sticky-nav');
-const navOffset = stickyNav.offsetHeight;
+const navOffset = stickyNav.offsetTop;
+const headerOffset = document.getElementById('header-wrapper').offsetHeight;
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY >= navOffset) {
+    if (window.scrollY >= navOffset + headerOffset) {
         stickyNav.classList.add('fixed');
         // nav 높이만큼 아래 콘텐츠 밀리지 않게
         document.body.style.paddingTop = stickyNav.offsetHeight + 'px';
