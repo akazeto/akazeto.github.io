1560e3c68d32563ede571d0136435fe9effb844a
akazeto <rlaenrl332@gmail.com>
2026-04-08 18:13:24 +0900

    61

--- assets/js/artworkFilter.js
+++ assets/js/artworkFilter.js
@@ -2,14 +2,13 @@ function filterArtwork(category) {
     const items = document.querySelectorAll('.gallery .artwork');
     items.forEach(item => {
         if (category === 'all' || !category || item.dataset.category === category) {
-            item.hidden = false;
+            item.style.display = 'flex';
         } else {
-            item.hidden = true;
+            item.style.display = 'none';
         }
     });
 }
 
-// 페이지가 완전히 로드된 후 실행
 window.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
     const category = urlParams.get('category');
