40f747d389e05965223efc31abf803b000ec76ea
akazeto <rlaenrl332@gmail.com>
2026-04-09 13:02:28 +0900

    64

--- assets/js/topButton.js
+++ assets/js/topButton.js
@@ -1,9 +1,11 @@
 const topBtn = document.getElementById('top-btn');
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 300) {
-        topBtn.style.display = 'block';
-    } else {
-        topBtn.style.display = 'none';
-    }
-});
+if (topBtn) {  // 버튼이 있을 때만 실행
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            topBtn.style.display = 'block';
+        } else {
+            topBtn.style.display = 'none';
+        }
+    });
+}
