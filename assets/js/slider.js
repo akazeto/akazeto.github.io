c597bb99b805e3819fbff4ab61594f6f5b23c794
akazeto <rlaenrl332@gmail.com>
2026-04-14 13:08:03 +0900

    80

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -25,7 +25,7 @@ if (cards.length > 0) {
     loadMore();
 
     window.addEventListener('scroll', () => {
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 300) {
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500) {
             if (loaded < total) loadMore();
         }
     });
