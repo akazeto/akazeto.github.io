b6673ace6b233728126d43e600ff9fd166aefa23
akazeto <rlaenrl332@gmail.com>
2026-06-09 11:47:35 +0900

    124

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,12 +3,13 @@ const modalImg = document.getElementById('modalImg');
 const modalClose = document.getElementById('modalClose');
 
 // 모달 열 때 top-btn 숨기기
-document.querySelectorAll('.image img, .gallery .artwork img').forEach(img => {
-    img.onclick = () => {
+document.querySelectorAll('.gallery .artwork').forEach(artwork => {
+    artwork.addEventListener('click', () => {
+        const img = artwork.querySelector('img');
         modalImg.src = img.src;
         modal.classList.add('active');
-        document.getElementById('top-btn').style.display = 'none';  // 추가
-    };
+        document.getElementById('top-btn').style.display = 'none';
+    });
 });
 
 // 닫기 버튼 또는 배경 클릭 시 닫기
